d5bba5ea498fa76c14b3b33263b13678317bf3a9
sverma091 <sverma@innovatrixinfotech.com>
2025-01-21 10:53:30 +0530

    user route has been added

--- backend/src/app/module/user/user.controller.ts
+++ backend/src/app/module/user/user.controller.ts
@@ -2,6 +2,21 @@ import { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import { User } from './user.model';
 import bcrypt from 'bcrypt'
+import nodemailer from 'nodemailer';
+
+require("dotenv").config();
+
+const amtp_email = process.env.SMTP_EMAIL;
+const smtp_name = process.env.SMTP_NAME;
+const pass = process.env.SMTP_PASSWORD;
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: amtp_email,
+        pass,
+    }
+} as any);
 
 export function generateToken(email: string) {
     const secret = process.env.JWT_SECRET;
@@ -30,8 +45,7 @@ export async function extractEmailId(token: string) {
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
-        const userinfo = req.body;
-        const { name, email, password } = userinfo;
+        const { name, email, password } = req.body;
         const isUser = await User.findOne({ email: email });
         if (isUser) return res.send({ message: "user already exist, please login" });
         const isRegister = await User.create({
@@ -50,8 +64,7 @@ export const registerUser = async (req: Request, res: Response) => {
 
 export const loginUser = async (req: Request, res: Response) => {
     try {
-        const userinfo = req.body;
-        const { email, password } = userinfo;
+        const { email, password } = req.body;
         const isUser = await User.findOne({ email: email });
         if (!isUser) return res.send({ message: "user not Valid" });
 
@@ -64,3 +77,49 @@ export const loginUser = async (req: Request, res: Response) => {
         return res.send({ message: "custom error", e });
     }
 }
+
+
+export const forgetPassword = async (req: Request, res: Response) => {
+    try {
+        const { email } = req.body;
+        const isUser = await User.findOne({ email: email });
+        if (!isUser) return res.send({ message: "email not Valid" });
+
+        const randomPass = `RN-${Date.now()}`;
+        const newPass = await bcrypt.hash(randomPass, 10);
+        const isSendPass = await isSend(email,randomPass);
+        isUser.password = newPass;
+        await isUser.save();
+
+        return res.status(200).send({
+            message: "new password has been send to the registered email",
+            isSendPass
+        });
+    } catch (e) {
+        return res.send({ message: "custom error", e });
+    }
+}
+
+
+export const isSend = async (email: string,randomPass:string) => {
+    try {
+        const mailOptions = {
+            from: `"${smtp_name}" <${email}>`,
+            to: email,
+            subject: "Your New Password",
+            text: randomPass,
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log('successfully sent to', email);
+        return {
+            message: `successfully sent to ${email}`
+        };
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return {
+            message: `Failed to send password to ${email}`,
+            error: error
+        };
+    }
+};
--- backend/src/app/module/user/user.route.ts
+++ backend/src/app/module/user/user.route.ts
@@ -1,9 +1,10 @@
 import express  from "express";
-import { loginUser, registerUser } from "./user.controller";
+import { forgetPassword, loginUser, registerUser } from "./user.controller";
 
 const userRotuer = express.Router();
 
 userRotuer.post('/register',registerUser);
 userRotuer.post('/login',loginUser);
+userRotuer.post('/forget-password',forgetPassword);
 
 export default userRotuer;
