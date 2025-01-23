dd00c6ac1032f90ee6cccc28d66ce71398f6808c
sverma091 <sverma@innovatrixinfotech.com>
2025-01-23 16:55:07 +0530

    cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,24 @@ import cors from "cors";
 import userRotuer from "./app/module/user/user.route";
 import taskRotuer from "./app/module/task/task.route";
 
+require('dotenv').config();
+
 const app: Application = express();
 //cors
-app.use(cors({origin:'*'}));
+
+const allowedOrigins = [process.env.FRONTENED_URL, 'http://localhost:3000'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
 // parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
